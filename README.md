3e33937f1162a3eeda11f0a7d917a90cbd538aff
AbdulrahmanAlosaimi <abdulrahmanosaimi@gmail.com>
2023-08-16 15:31:16 +0300

    Renamed app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TestApp
+# angular-fundamentals
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.5.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,10 +3,12 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
+    })
+  );
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TestApp'`, () => {
+  it(`should have as title 'angular-fundamentals'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('TestApp');
+    expect(app.title).toEqual('angular-fundamentals');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('TestApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'angular-fundamentals app is running!'
+    );
   });
 });
