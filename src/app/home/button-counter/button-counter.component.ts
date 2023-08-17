46f7f5b8c4aa05c6cd1559c2d3696cc906ce285c
Ali Habibullah <64721459+ALLIA12@users.noreply.github.com>
2023-08-17 14:48:47 +0300

    Task 4 part 4 and 5

--- src/app/home/button-counter/button-counter.component.ts
+++ src/app/home/button-counter/button-counter.component.ts
@@ -7,8 +7,18 @@ import { Component } from '@angular/core';
 })
 export class ButtonCounterComponent {
   counter: number = 0;
+  increasedByFilteredEvent: boolean = false;
 
-  increaseCounter(): void {
-    this.counter++;
+  increaseCounter(event: MouseEvent): void {
+    if (event.shiftKey) {
+      this.counter++;
+      this.increasedByFilteredEvent = true;
+
+      setTimeout(() => {
+        this.increasedByFilteredEvent = false;
+      }, 3000); // Hide message after 3000 milliseconds (3 seconds)
+    } else {
+      this.increasedByFilteredEvent = false;
+    }
   }
 }
